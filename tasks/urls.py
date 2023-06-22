b84c2394b20c2fceb7705a89204885fb7063ec52
Захар <carroll.lewi@gmail.com>
2023-06-22 17:09:22 +0300

    create new model

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import index_view, about_view, create_view, detail_view, update_view, delete_view
+from .views import list_view, about_view, create_view, detail_view, update_view, delete_view
 
 
 app_name = 'tasks'
 
 urlpatterns = [
-    path('', index_view, name='short_index'),
-    path('tasks/', index_view, name='index'),
+    path('', list_view, name='short_index'),
+    path('tasks/', list_view, name='index'),
     path('about/', about_view, name='about'),
     path('create/', create_view, name='create'),  # Сторінка створення завдання
     path('<uuid:uuid>/', detail_view, name='detail'),  # Перегляд деталей одного завдання.

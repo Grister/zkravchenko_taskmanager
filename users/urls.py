20b496948701440a30990645729f998bbcfcf2f3
Захар <carroll.lewi@gmail.com>
2023-07-09 19:01:55 +0300

    refactored views to Class-based views and updated forms

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import sign_up_view, sign_in_view, logout_view
-
+from .views import UserRegistrationView, UserLogoutView, UserLoginView
 
 app_name = 'users'
 
 urlpatterns = [
-    path('sign-up/', sign_up_view, name='sign_up'),  # Сторінка реєстрації користувача
-    path('sign-in/', sign_in_view, name='sign_in'),  # Сторінка входу користувача
-    path('logout/', logout_view, name='logout'),  # Сторінка виходу користувача
+    path('sign-up/', UserRegistrationView.as_view(), name='sign_up'),  # Сторінка реєстрації користувача
+    path('sign-in/', UserLoginView.as_view(), name='sign_in'),  # Сторінка входу користувача
+    path('logout/', UserLogoutView.as_view(), name='logout'),  # Сторінка виходу користувача
 ]

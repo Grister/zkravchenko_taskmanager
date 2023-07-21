070f8f91c7543a1934096d8058bddc92f9626100
Захар <carroll.lewi@gmail.com>
2023-07-21 13:20:25 +0300

    add validate email for UserSerializer

--- api/users/serializers.py
+++ api/users/serializers.py
@@ -9,6 +9,14 @@ class UserSerializer(serializers.ModelSerializer):
         model = UserModel
         fields = ["id", "username", "first_name", "last_name", "email"]
 
+    def validate_email(self, value):
+        instance = self.instance
+
+        if instance and instance.email != value and UserModel.objects.filter(email=value).exists():
+            raise serializers.ValidationError("This email is already in use.")
+
+        return value
+
 
 class UserCreateSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(required=True)
